213f0a097c99258c4cc078386121f353f47b2fa6
Yang Jiang <1473356847@qq.con>
2020-07-25 09:23:01 +0800

    第一次提交

---
+++ src/main/java/com/accp/ShangchuanApplication.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangchuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShangchuanApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/accp/biz/yj.java
@@ -0,0 +1,8 @@
+package com.accp.biz;
+
+public class yj {
+		public void s() {
+			System.out.println("第一次提交");
+			System.out.println("第一次提交");
+		}
+}
